055041d0bf99b6f74888a5dde6c84294d73d8e1a
Aniket <144130684+Aniketsharma09@users.noreply.github.com>
2025-07-24 12:08:13 +0530

    Update MainRoutes.jsx

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -4,7 +4,9 @@ import ScrollToTop from "./ScrollToTop";
 const Home = lazy(() => import("../pages/Home/Home"));
 const AnarcWatch = lazy(() => import("../pages/Anarc-Watch/AnarcWatch"));
 const Products = lazy(() => import("../pages/Products/Products"));
-const ProductDetails = lazy(() => import("../pages/Product-Detail/productDetails"));
+const ProductDetails = lazy(() =>
+  import("../pages/Product-Detail/ProductDetails")
+);
 const OurStory = lazy(() => import("../pages/Our-Story/OurStory"));
 const Login = lazy(() => import("../pages/Login/login"));
 const SignUp = lazy(() => import("../pages/SignUP/signUp"));
@@ -14,13 +16,62 @@ const MainRoutes = () => {
     <>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
-        <Route path="/anarc-watch" element={<Suspense fallback={<div>Loading...</div>}><AnarcWatch /></Suspense>} />
-        <Route path="/products" element={<Suspense fallback={<div>Loading...</div>}><Products /></Suspense>} />
-        <Route path="/product-detail/:id" element={<Suspense fallback={<div>Loading...</div>}><ProductDetails /></Suspense>} />
-        <Route path="/our-story" element={<Suspense fallback={<div>Loading...</div>}><OurStory /></Suspense>} />
-        <Route path="/login" element={<Suspense fallback={<div>Loading...</div>}><Login /></Suspense>} />
-        <Route path="/signUp" element={<Suspense fallback={<div>Loading...</div>}><SignUp /></Suspense>} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/anarc-watch"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <AnarcWatch />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Products />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/product-detail/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ProductDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/our-story"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <OurStory />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signUp"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignUp />
+            </Suspense>
+          }
+        />
       </Routes>
     </>
   );
