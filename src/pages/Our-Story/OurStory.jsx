76dafca83f7efc1390a6897cdfca3d1c785ab1fe
Aniket <144130684+Aniketsharma09@users.noreply.github.com>
2025-07-25 11:08:32 +0530

    Design Fixes

--- src/pages/Our-Story/OurStory.jsx
+++ src/pages/Our-Story/OurStory.jsx
@@ -1,17 +1,23 @@
-import React, { lazy, Suspense } from "react";
-
+import { lazy, Suspense, useRef } from "react";
 const Section1 = lazy(() => import("./Sections/Section1"));
 const Section2 = lazy(() => import("./Sections/Section2"));
 
 const OurStory = () => {
+  const section2Ref = useRef(null); 
+
+  const scrollToSection2 = () => {
+    section2Ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="ourStoryMain">
       <Suspense fallback={<div>Loading Section 1...</div>}>
-        <Section1 />
+        <Section1 scrollToSection2={scrollToSection2} />
       </Suspense>
-
       <Suspense fallback={<div>Loading Section 2...</div>}>
-        <Section2 />
+        <div ref={section2Ref}>
+          <Section2 />
+        </div>
       </Suspense>
     </div>
   );
--- src/pages/Our-Story/Sections/Section1.jsx
+++ src/pages/Our-Story/Sections/Section1.jsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
 import "../ourStory.css";
 import useSection1Animation from "../SectionAnimations/useSection1Animation";
+import { useNavigate } from "react-router-dom";
 
-const Section1 = () => {
+const Section1 = ({ scrollToSection2 }) => {
+  const navigate = useNavigate();
   const containerRef = useRef(null);
 
   useSection1Animation(containerRef);
@@ -20,10 +22,12 @@ const Section1 = () => {
           me on this journey of innovation, creation, and making an impact in
           the digital world.
         </p>
+
         <div className="button-cont">
-          <button>explore my journey</button>
-          <button>view products</button>
+          <button onClick={scrollToSection2}>explore my journey</button>
+          <button onClick={() => navigate("/products")}>view products</button>
         </div>
+
         <div className="content-info">
           <div className="cont-info">
             <h1>12M +</h1>
@@ -39,7 +43,11 @@ const Section1 = () => {
           </div>
         </div>
       </div>
-      <img src="https://www.layers.shop/cdn/shop/files/gallery_img4.webp?v=1725254041" />
+
+      <img
+        src="https://www.layers.shop/cdn/shop/files/gallery_img4.webp?v=1725254041"
+        alt="Shlok"
+      />
     </div>
   );
 };
